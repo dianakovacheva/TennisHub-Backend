3166a03f7567a986dae9e37caa5d8d2db09153ab
Diana Kovacheva <dmarinova@outlook.com>
2023-11-19 20:44:25 +0100

    Update club related routes

--- router/club.js
+++ router/club.js
@@ -1,39 +1,26 @@
 const express = require("express");
 const router = express.Router();
 const { auth } = require("../utils");
-const {
-  clubController,
-  courtController,
-  commentController,
-} = require("../controllers");
+const { clubController, courtController } = require("../controllers");
 
 // Club Related Routes
-router.post("/club/create", auth(), clubController.createClub);
-router.get("/club/:clubId", auth(), clubController.getClubById);
-router.put("/club/:clubId/edit", auth(), clubController.editClub);
-router.delete("/club/:clubId/delete", auth(), clubController.deleteClub);
-router.put("/club/:clubId/join", auth(), clubController.joinClub);
-router.delete("/club/:clubId/leave", auth(), clubController.leaveClub);
-router.get("/club/:clubId/members", auth(), clubController.getClubMembers);
+router.post("/create", auth(), clubController.createClub);
+router.get("/:clubId", auth(), clubController.getClubById);
+router.put("/:clubId/edit", auth(), clubController.editClub);
+router.delete("/:clubId/delete", auth(), clubController.deleteClub);
+router.put("/:clubId/join", auth(), clubController.joinClub);
+router.delete("/:clubId/leave", auth(), clubController.leaveClub);
+router.get("/:clubId/members", auth(), clubController.getClubMembers);
+router.get("/", clubController.getAllClubs);
 
 // Court Related Routes
-router.post("/club/courts/create", auth(), courtController.createCourt);
-router.put("/club/courts/:courtId/edit", auth(), courtController.editCourt);
-router.delete(
-  "/club/courts/:courtId/delete",
-  auth(),
-  courtController.deleteCourt
-);
-router.get(
-  "/club/:clubId/courts/:courtId",
-  auth(),
-  clubController.getCourtById
-);
-router.get("/club/:clubId/courts", auth(), clubController.getClubCourts);
+router.post("/court/create", auth(), courtController.createCourt);
+router.put("/court/:courtId/edit", auth(), courtController.editCourt);
+router.delete("/court/:courtId/delete", auth(), courtController.deleteCourt);
+router.get("/:clubId/court/:courtId", auth(), clubController.getCourtById);
+router.get("/:clubId/courts", auth(), clubController.getClubCourts);
 
 // Search Related Route
 router.get("/search", clubController.search);
-
-router.get("/", clubController.getAllClubs);
 
 module.exports = router;
