cedfc9c08de584210e6534e0174ebe5e3e07ffaa
Diana Kovacheva <dmarinova@outlook.com>
2023-11-19 17:13:44 +0100

    Add booking related routes
    Get booking by id
    Edit booking
    Get all bookings

--- router/booking.js
+++ router/booking.js
@@ -4,6 +4,9 @@ const { auth } = require("../utils");
 const { bookingController } = require("../controllers");
 
 router.post("/bookCourt", auth(), bookingController.bookCourt);
+router.get("/:bookingId", auth(), bookingController.getBookingById);
+router.put("/:bookingId/edit", auth(), bookingController.editBooking);
 router.delete("/:bookingId/delete", auth(), bookingController.deleteBooking);
+router.get("/", bookingController.getAllBookings);
 
 module.exports = router;
