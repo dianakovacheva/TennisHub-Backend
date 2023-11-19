143a5526616ceb3c538829872c254de0d9bdd008
Diana Kovacheva <dmarinova@outlook.com>
2023-11-19 17:13:04 +0100

    Add get user bookings route

--- router/user.js
+++ router/user.js
@@ -6,6 +6,6 @@ const { userController, bookingController } = require("../controllers");
 router.get("/:userId", auth(), userController.getUserById);
 router.put("/:userId", auth(), userController.editUserInfo);
 router.get("/:userId/clubs", auth(), userController.getUserCreatedClubs);
-// router.get("/:userId/bookings", auth(), bookingController.getUserBookings);
+router.get("/:userId/bookings", auth(), bookingController.getUserBookings);
 
 module.exports = router;
