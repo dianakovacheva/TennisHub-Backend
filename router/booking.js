6084bec043d061d09c863fd610b7f570dbd70e01
Diana Kovacheva <dmarinova@outlook.com>
2023-11-21 14:17:52 +0100

    Change the route for court booking

--- router/booking.js
+++ router/booking.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const { auth } = require("../utils");
 const { bookingController } = require("../controllers");
 
-router.post("/bookCourt", auth(), bookingController.bookCourt);
-router.get("/:bookingId", auth(), bookingController.getBookingById);
+router.post("/book-court", auth(), bookingController.bookCourt);
+router.get("/:bookingId", bookingController.getBookingById);
 router.put("/:bookingId/edit", auth(), bookingController.editBooking);
 router.delete("/:bookingId/delete", auth(), bookingController.deleteBooking);
 router.get("/", bookingController.getAllBookings);
