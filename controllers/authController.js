2d1e948e684e4731183767c5b41eab7e1c415014
Diana Kovacheva <dmarinova@outlook.com>
2023-12-07 00:32:22 +0100

    Update authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -69,7 +69,10 @@ function login(req, res, next) {
           secure: true,
         });
       } else {
-        res.cookie(authCookieName, token, { httpOnly: true });
+        res.cookie(authCookieName, token, {
+          httpOnly: true,
+          expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 365),
+        });
       }
       res.status(200).send(user);
     })
